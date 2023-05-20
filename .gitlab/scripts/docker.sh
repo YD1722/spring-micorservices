d18dddd53d5cd8e02b14773761ed24655ac58e81
yashodhah <y.dominatz@gmail.com>
2023-05-21 00:36:34 +0530

    update CI/CD configs

--- .gitlab/scripts/docker.sh
+++ .gitlab/scripts/docker.sh
@@ -7,21 +7,21 @@ for service_dir in artifacts/*; do
     ecr_tag="008744601422.dkr.ecr.ap-south-1.amazonaws.com/$service_name:latest"
 
     # Create the Dockerfile
-    dockerfile_content="FROM openjdk:17-alpine\nMAINTAINER yashodha.dev.space\nCOPY $service_name/$service_name.jar app.jar\nENTRYPOINT [\"java\", \"-jar\", \"/app.jar\"]"
+    dockerfile_content="FROM openjdk:17-alpine\nMAINTAINER yashodha.dev.space\nCOPY artifacts/$service_name/$service_name.jar app.jar\nENTRYPOINT [\"java\", \"-jar\", \"/app.jar\"]"
     echo -e "$dockerfile_content" > Dockerfile
 
     pwd
-    ll
+    ls
     cat Dockerfile
 
-#    # Build the Docker image
-#    docker build \
-#      --build-arg SERVICE_NAME="$service_name" \
-#      --tag "$tag" \
-#      .
-#
-#    # Tag the Docker image dynamically
-#    docker tag "$tag" "$ecr_tag"
+    # Build the Docker image
+    docker build \
+      --build-arg SERVICE_NAME="$service_name" \
+      --tag "$tag" \
+      .
+
+    # Tag the Docker image dynamically
+    docker tag "$tag" "$ecr_tag"
 
     # Clean up the Dockerfile
     rm Dockerfile
