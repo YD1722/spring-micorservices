6d811a65dac3cc5f7f14f2ace8d8d38d8aea7d4c
yashodhah <y.dominatz@gmail.com>
2023-05-20 15:55:47 +0530

    update CI/CD

--- .gitlab/scripts/docker.sh
+++ .gitlab/scripts/docker.sh
@@ -6,10 +6,20 @@ for service_dir in artifacts/*; do
     tag="$service_name:latest"
     ecr_tag="008744601422.dkr.ecr.ap-south-1.amazonaws.com/$service_name:latest"
 
-    # Build the Docker image with the provided SERVICE_NAME as a build argument
-    docker build --build-arg SERVICE_NAME="$service_name" -t "$tag" -f .gitlab/Dockerfile .
+    # Create the Dockerfile
+    dockerfile_content="FROM openjdk:17-alpine\nMAINTAINER yashodha.dev.space\nCOPY $service_name/$service_name.jar app.jar\nENTRYPOINT [\"java\", \"-jar\", \"/app.jar\"]"
+    echo -e "$dockerfile_content" > Dockerfile
+
+    # Build the Docker image
+    docker build \
+      --build-arg SERVICE_NAME="$service_name" \
+      --tag "$tag" \
+      .
 
     # Tag the Docker image dynamically
     docker tag "$tag" "$ecr_tag"
+
+    # Clean up the Dockerfile
+    rm Dockerfile
   fi
 done
