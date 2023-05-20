d3bbfea36399fcf2882d9c7fbb2a935b9266a470
yashodhah <y.dominatz@gmail.com>
2023-05-20 15:35:40 +0530

    update CI/CD

--- .gitlab/scripts/docker.sh
+++ .gitlab/scripts/docker.sh
@@ -3,7 +3,6 @@
 service_name=$1
 
 pwd
-ls
 
 docker build --build-arg SERVICE_NAME="$service_name" -t "$service_name" .gitlab
-docker tag "$service_name":latest 008744601422.dkr.ecr.ap-south-1.amazonaws.com/"$service_name":latest
+docker tag "$service_name:latest" "008744601422.dkr.ecr.ap-south-1.amazonaws.com/$service_name:latest"
