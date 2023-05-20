f62c1cc2782a39756ca90c7fdcd9bf848d4e7be1
yashodhah <y.dominatz@gmail.com>
2023-05-20 17:29:07 +0530

    fix rabbit mq consume

--- image-processing-service/src/main/java/com/example/process/service/RabbitMQConsumer.java
+++ image-processing-service/src/main/java/com/example/process/service/RabbitMQConsumer.java
@@ -2,12 +2,20 @@ package com.example.process.service;
 
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 
-@RabbitListener(queues = "${rabbit.queueName}")
+// TODO: Class level listeners not working ??
+//@RabbitListener(queues = "${app.queue.name}")
+@Component
 public class RabbitMQConsumer {
 
-    @RabbitHandler
-    public void receive(String message) {
+//    @RabbitHandler
+//    public void receive(String message) {
+//        System.out.println("Received Message From RabbitMQ: " + message);
+//    }
+
+    @RabbitListener(queues = {"${app.queue.name}"})
+    public void consume(String message) {
         System.out.println("Received Message From RabbitMQ: " + message);
     }
 }
