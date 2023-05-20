c6d94927a85df0eeb32c0def3b36cf08db75154d
yashodhah <y.dominatz@gmail.com>
2023-05-20 15:42:25 +0530

    update CI/CD

--- .gitlab/scripts/docker.sh
+++ .gitlab/scripts/docker.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-service_name=$1
+for service_dir in artifacts/*; do
+  if [ -d "$service_dir" ]; then
+    service_name=$(basename "$service_dir")
+    tag="$service_name:latest"
+    ecr_tag="008744601422.dkr.ecr.ap-south-1.amazonaws.com/$service_name:latest"
 
-pwd
+    # Print the service name
+    echo "Service Name: $service_name"
 
-docker build --build-arg SERVICE_NAME="$service_name" -t "$service_name" .gitlab
-docker tag "$service_name:latest" "008744601422.dkr.ecr.ap-south-1.amazonaws.com/$service_name:latest"
+    # Build the Docker image with the provided SERVICE_NAME as a build argument
+    docker build --build-arg SERVICE_NAME="$service_name" -t "$tag" "$service_dir"
+
+    # Tag the Docker image dynamically
+    docker tag "$tag" "$ecr_tag"
+  fi
+done
