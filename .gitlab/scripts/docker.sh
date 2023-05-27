db4fce3171c3fb559f825f5979e00f687c8f1c46
yashodhah <y.dominatz@gmail.com>
2023-05-28 00:43:09 +0530

    update docker image push

--- .gitlab/scripts/docker.sh
+++ .gitlab/scripts/docker.sh
@@ -6,8 +6,6 @@ for service_dir in artifacts/*; do
     tag="$service_name:latest"
     ecr_tag="008744601422.dkr.ecr.ap-south-1.amazonaws.com/$service_name:latest"
 
-    echo $ecr_tag
-
     # Create the Dockerfile
     dockerfile_content="FROM openjdk:17-alpine\nMAINTAINER yashodha.dev.space\nCOPY artifacts/$service_name/$service_name.jar app.jar\nENTRYPOINT [\"java\", \"-jar\", \"/app.jar\"]"
     echo -e "$dockerfile_content" > Dockerfile
@@ -19,7 +17,6 @@ for service_dir in artifacts/*; do
       .
 
     docker tag "$tag" "$ecr_tag"
-    docker images
     docker push "$ecr_tag"
 
     # Clean up the Dockerfile
