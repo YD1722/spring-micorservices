a24eb9962763e140ce9ff96305401659ec2dae69
yashodhah <y.dominatz@gmail.com>
2023-11-23 21:27:21 +0800

    add kafka related changes

---
+++ ai-art-consumer/src/main/java/com/example/aiartconsumer/AiArtConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiartconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiArtConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiArtConsumerApplication.class, args);
+    }
+
+}
